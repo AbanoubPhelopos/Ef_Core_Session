ddde190ecd8c628a4d6952c39ec59c6c0012dcc4
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-01-31 14:41:22 +0200

    student model and db onCongig

---
+++ Ef_Core_Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Ef_Core_Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ef_Core_Application.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options
+                .UseSqlServer("Server=DESKTOP-TFU0IME\\SQLEXPRESS;" +
+                                    "Database=EfCore;Trusted_Connection=True;" +
+                                        "TrustedServerCertificate=true;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Ef_Core_Application/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Ef_Core_Application.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Level { get; set; }
+    }
+}
